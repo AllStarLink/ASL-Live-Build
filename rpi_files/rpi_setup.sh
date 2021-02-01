0c6372282cb8188fde2c7ec74dc4c73977c78519
Rob Vella <me@robvella.com>
2021-02-01 21:37:36 +0000

    Refactor to do post build operations on both builds

--- rpi_files/rpi_setup.sh
+++ rpi_files/rpi_setup.sh
@@ -11,9 +11,6 @@ echo ####################################
 # Including common functions
 [ -e "${LIVE_BUILD}/scripts/build.sh" ] && . "${LIVE_BUILD}/scripts/build.sh" || . /usr/lib/live/build.sh
 
-# ASL Functions
-. scripts/functions.sh
-
 # Checking stage file
 Check_stagefile .build/binary_rpi
 
@@ -23,23 +20,16 @@ install -m 644 rpi_files/fstab $ROOTFS/etc
 install -m 644 rpi_files/hosts $ROOTFS/etc/hosts
 install -m 755 rpi_files/resize2fs_once $ROOTFS/etc/init.d
 
-mv $ROOTFS/etc/asterisk $ROOTFS/etc/asterisk.old
-cp -a asterisk $ROOTFS/etc/asterisk
-
 # Always enable SSH
 touch $BOOTFS/ssh
 
 HOST=repeater
-
-openDiskImage
 
 sed -i "s/BOOTDEV/PARTUUID=${DISKUUID}-01/" "${ROOTFS}/etc/fstab"
 sed -i "s/ROOTDEV/PARTUUID=${DISKUUID}-02/" "${ROOTFS}/etc/fstab"
 sed -i "s/ROOTDEV/PARTUUID=${DISKUUID}-02/" "${BOOTFS}/cmdline.txt"
 sed -i "s/localhost\.localdomain/${HOST}/" "${ROOTFS}/etc/hostname"
 sed -i "s/localhost/${HOST}/" "${ROOTFS}/etc/hosts"
-
-cp rpi_files/asterisk.service $ROOTFS/etc/systemd/system
 
 cat <<EOT > $ROOTFS/setup.sh
 #!/bin/bash
@@ -56,22 +46,13 @@ done
 
 systemctl enable regenerate_ssh_host_keys
 systemctl enable resize2fs_once
-systemctl enable asterisk
-systemctl disable asl-asterisk
 systemctl disable hciuart.service
 systemctl disable bluealsa.service
 systemctl disable bluetooth.service
 EOT
 
-chmod 755 $ROOTFS/setup.sh
-chroot $ROOTFS /setup.sh
+chroot $ROOTFS bash /setup.sh
 rm $ROOTFS/setup.sh
-
-closeDiskImage
-
-#zip the image
-rm -f $DISKIMAGE.zip 2>/dev/null
-zip $DISKIMAGE.zip $DISKIMAGE
 
 # Creating stage file
 Create_stagefile .build/binary_rpi
--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -7,4 +7,22 @@ echo #                                  #
 echo #                                  #
 echo ####################################
 
+mv $ROOTFS/etc/asterisk $ROOTFS/etc/asterisk.old
+cp -a asterisk $ROOTFS/etc/asterisk
+
+cp scripts/asterisk.service $ROOTFS/etc/systemd/system
+
+cat <<EOT > $ROOTFS/setup.sh
+#!/bin/bash
+systemctl disable asl-asterisk
+systemctl enable asterisk
+sed -i 's/https:\/\/kc1kcc\.com/http:\/\/apt\.allstarlink\.org/g' /etc/apt/sources.list.d/live.list
+EOT
+
+chroot $ROOTFS bash /setup.sh
+rm $ROOTFS/setup.sh
+
+# Finally, zip the image
+rm -f $DISKIMAGE.zip 2>/dev/null
+zip $DISKIMAGE.zip $DISKIMAGE
 

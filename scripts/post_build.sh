a1f6fe8882cf437832ad60f5dd3f147f2cb2aa1e
Rob Vella <me@robvella.com>
2021-02-02 18:37:19 -0800

    Move towards hooks instead

--- scripts/post_build.sh
+++ scripts/post_build.sh
@@ -7,17 +7,6 @@ echo #                                  #
 echo #                                  #
 echo ####################################
 
-cat <<EOT > $ROOTFS/setup.sh
-#!/bin/bash
-systemctl disable asl-asterisk
-systemctl enable asterisk
-apt -y remove auto-apt-proxy
-sed -i 's/https:\/\/kc1kcc\.com/http:\/\/apt\.allstarlink\.org/g' /etc/apt/sources.list.d/live.list
-EOT
-
-chroot $ROOTFS bash /setup.sh
-rm $ROOTFS/setup.sh
-
 # Finally, zip the image
 rm -f $DISKIMAGE.zip 2>/dev/null
 zip $DISKIMAGE.zip $DISKIMAGE

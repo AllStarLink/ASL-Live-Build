0edca6516ab0dca7d8059f064e7ecf23a147631b
Rob Vella <me@robvella.com>
2021-02-02 19:51:23 -0800

    Restore rpi changes

--- rpi_files/rpi_setup.sh
+++ rpi_files/rpi_setup.sh
@@ -14,6 +14,31 @@ echo ####################################
 # Checking stage file
 Check_stagefile .build/binary_rpi
 
+DISKIMAGE="$(cat config/build | grep ^Name:.* | sed 's/^Name: \(.*\)$/\1/g')-${1}.img"
+OFFSET=$(expr $(fdisk -u -l $DISKIMAGE | sed -ne "s|^${DISKIMAGE}1[ *]*\([0-9]*\).*|\1|p") '*' 512)
+OFFSETm1=$(expr $OFFSET '-' 1)
+
+parted -s $DISKIMAGE rm 1
+parted -s $DISKIMAGE unit B mkpart primary fat32 4194304 $OFFSETm1
+parted -s $DISKIMAGE unit B mkpart primary ext4 $OFFSET 100%
+
+DEVB=$(losetup -f)
+losetup -o 4194304 --sizelimit $(expr $OFFSET '-' 4194304) $DEVB $DISKIMAGE
+DEVR=$(losetup -f)
+losetup -o $OFFSET $DEVR $DISKIMAGE
+mkdosfs -n boot -F 32 -v $DEVB
+DISKUUID="$(dd if=$DISKIMAGE skip=440 bs=1 count=4 2>/dev/null | xxd -e | cut -f 2 -d' ')"
+ROOTFS=rootfs
+BOOTFS=bootfs
+
+mkdir $ROOTFS || true
+mkdir $BOOTFS || true
+
+mount $DEVR $ROOTFS
+mount $DEVB $BOOTFS
+
+mv $ROOTFS/boot/* $BOOTFS
+
 install -m 644 rpi_files/config.txt $BOOTFS
 install -m 644 rpi_files/cmdline.txt $BOOTFS
 install -m 644 rpi_files/fstab $ROOTFS/etc
@@ -56,3 +81,16 @@ rm $ROOTFS/setup.sh
 
 # Creating stage file
 Create_stagefile .build/binary_rpi
+
+umount $ROOTFS
+umount $BOOTFS
+
+losetup -d $DEVR
+losetup -d $DEVB
+
+rm -rf $ROOTFS || true
+rm -rf $BOOTFS || true
+
+# Finally, zip the image
+rm -f $DISKIMAGE.zip 2>/dev/null
+zip $DISKIMAGE.zip $DISKIMAGE

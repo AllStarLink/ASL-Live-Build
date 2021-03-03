0e1118714a6e8e35275f63db473c84f8c7f4a37f
Rob Vella <me@robvella.com>
2021-03-03 14:46:03 -0800

    Auto-update motd and issue every 15 mins via cron

--- config/includes.chroot/usr/local/sbin/update-motd-issue.sh
+++ config/includes.chroot/usr/local/sbin/update-motd-issue.sh
@@ -21,7 +21,7 @@ echo -e "
 │        By: Jim WB6NIL (SK), Steve N4IRS, Adam KC1KCC, Rob KK9ROB, and others          │
 │                            https://www.allstarlink.org                                │
 │                           (C) 1999 - 2005, Digium, Inc                                │
-│                             (C) 2021 AllStarLink, Inc                                 │
+│                          (C) 2018-2021 AllStarLink, Inc                               │
 └───────────────────────────────────────────────────────────────────────────────────────┘
 You may access this machine via SSH via the following hosts:
 
